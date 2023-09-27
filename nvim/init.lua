07651abe7ac91f83543266240beec718a178eeb2
Majors <majors@majors.email>
2023-09-26 20:50:24 -0700

    Add volar vue/ts/js LSP to nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -202,6 +202,9 @@ lspconfig['lua_ls'].setup {
   }
 }
 lspconfig['pylsp'].setup{}
+lspconfig['volar'].setup{
+  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
+}
 
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
