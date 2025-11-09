f02ede85e2182b480998a142d0e721f72a4688cd
majors <majors@majors.email>
2025-11-09 15:08:30 -0800

    remove deprecated add_default_mappings for nvim leap

--- nvim/init.lua
+++ nvim/init.lua
@@ -114,7 +114,8 @@ require("mason-lspconfig").setup {
 	}
 }
 
-require('leap').add_default_mappings()
+vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
 
 require('overseer').setup()
 
